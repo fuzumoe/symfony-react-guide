6b3b579a455c1721ebb20da0cb6b236c6650e4a4
Adam Fuzum Tewelde <tewelde@debitos.com>
2020-11-19 16:56:17 +0100

    php4 guide

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # symfony-react-guide
+
+## start project
+1. docker-compuse up
+   or
+2. php -S localhost:8080 public/index.php
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '591d8c7f88f988ae5264dc1d216e1c9f3513f64a',
+    'name' => 'php-basics/php-7',
+  ),
+  'versions' => 
+  array (
+    'php-basics/php-7' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '591d8c7f88f988ae5264dc1d216e1c9f3513f64a',
+    ),
+  ),
+);
